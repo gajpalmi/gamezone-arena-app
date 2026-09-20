0be69b8610d09c2dc6ff4f2fca40001b0b145e2b
Mishrwangajpal33 <63528085-mishrwangajpal3@users.noreply.replit.com>
2026-09-20 10:59:01 +0000

    Generate Android native project for APK build

--- artifacts/gamezone-arena/android/app/src/main/java/com/gamezonearena/app/MainActivity.kt
+++ artifacts/gamezone-arena/android/app/src/main/java/com/gamezonearena/app/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.gamezonearena.app
+import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
@@ -10,6 +11,9 @@ import expo.modules.ReactActivityDelegateWrapper
 class MainActivity : ReactActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
+    SplashScreenManager.registerOnActivity(this)
+    // @generated end expo-splashscreen
         super.onCreate(null)
     }
 
